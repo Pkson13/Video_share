c61a8d1a67b4a3aaa2125732c3586c804890e237
Peterson <kinyanjuipeterson12787@gmail.com>
2025-04-09 02:58:24 +0300

    starting point

---
+++ index.js
@@ -0,0 +1,18 @@
+import https from "https";
+import express from "express";
+import { Server } from "socket.io";
+import { readFileSync } from "node:fs";
+
+const app = express();
+// https.createServer(app, (req,res)=>{
+// })
+
+const options = {
+  key: readFileSync("./cert-key.pem"),
+  cert: readFileSync("./cert.pem"),
+};
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+https.createServer(options, app).listen(8000);
---
+++ tasklist.md
@@ -0,0 +1,6 @@
+> mkcert cert
+> Created a new certificate valid for the following names 📜
+
+- "cert"
+  The certificate is at "./cert.pem" and the key at "./cert-key.pem" ✅
+  It will expire on 9 July 2027 🗓
